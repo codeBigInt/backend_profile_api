9b1528fb7a604fa1cfbd699a1726eee41ed5686a
codeBigInt <elliotlucky99@mail.com>
2025-01-30 20:40:30 +0100

    fix: fixed email address

--- controllers/fetchData.js
+++ controllers/fetchData.js
@@ -2,7 +2,7 @@ import { getUTCTime } from "../utils/getUTCTime.js"
 
 export const fetchData = (req, res) => {
     const data = {
-        email: "elliottlucky@gmail.com",
+        email: "elliotlucky509@gmail.com",
         current_datetime: getUTCTime(),
         github_url: "https://github.com/codeBigInt/backend_profile_api"
     }
