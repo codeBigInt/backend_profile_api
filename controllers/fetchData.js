203fb57725bd59c4acde90b7dcbdaa6f240a9b5d
codeBigInt <elliotlucky99@mail.com>
2025-01-30 20:19:52 +0100

    fix: fixed github url

--- controllers/fetchData.js
+++ controllers/fetchData.js
@@ -4,7 +4,7 @@ export const fetchData = (req, res) => {
     const data = {
         email: "elliottlucky@gmail.com",
         current_datetime: getUTCTime(),
-        github_url: "https://github.com/codeBigInt/backend_profile_api.git"
+        github_url: "https://github.com/codeBigInt/backend_profile_api"
     }
     res.status(200).send(data)
 }
