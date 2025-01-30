217dcc1f6878397ba6c0aa3bc40d7cbd76669237
codeBigInt <elliotlucky99@mail.com>
2025-01-30 19:05:48 +0100

    chore: configured vercel

--- index.js
+++ index.js
@@ -15,3 +15,6 @@ app.use("/", router)
 app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 })
+
+
+export default app;
--- utils/config.js
+++ utils/config.js
@@ -1,1 +1,5 @@
+import * as dotenv from "dotenv"
+
+dotenv.config({ path: ["./*"] })
+
 export const PORT = process.env.PORT || 3000
